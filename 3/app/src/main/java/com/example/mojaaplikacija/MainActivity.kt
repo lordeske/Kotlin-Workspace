33b2d37b7d5ab0ebbbdaa33f7c21f09f29a1a54d
Mihajlo Eskic <eskicmihajlo55@gmail.com>
2024-04-08 12:05:51 +0200

    wotking with composible

--- 3/app/src/main/java/com/example/mojaaplikacija/MainActivity.kt
+++ 3/app/src/main/java/com/example/mojaaplikacija/MainActivity.kt
@@ -13,15 +13,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mojaaplikacija.ui.theme.MojaAplikacijaTheme
@@ -49,6 +55,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeLayout()
 {
+    var  amountInput by remember {
+        mutableStateOf("")
+    }
+
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+
+    val tip = calucalteTip(amount)
+
     Column (modifier = Modifier
         .statusBarsPadding()
         .padding(horizontal = 40.dp)
@@ -65,10 +79,12 @@ fun TipTimeLayout()
                 .align(alignment = Alignment.Start))
 
 
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(value = amountInput, onValueChange = {amountInput = it},modifier = Modifier
+            .padding(bottom = 32.dp)
+            .fillMaxWidth())
 
         
-        Text(text = stringResource(id = R.string.tip_amount, "0.00$"),
+        Text(text = stringResource(id = R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall)
         
         Spacer(modifier = Modifier.height(150.dp))
@@ -95,10 +111,17 @@ fun calucalteTip(cenaRacuna : Double , tipPostotka : Double = 15.0): String {
 
 
 @Composable
-fun EditNumberField(modifier: Modifier = Modifier)
-{
+fun EditNumberField(value : String ,onValueChange:(String) -> Unit, modifier: Modifier = Modifier) {
 
-    var inputAmount = mutableSetOf("0")
 
-    TextField(value = inputAmount.value, onValueChange = {}, modifier = modifier)
+
+
+    TextField(value = value, onValueChange = onValueChange ,
+        modifier = modifier,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label = { Text(stringResource(R.string.bill_amount))})
+
+
+
 }
