34c79669075e7ecd488388f4f710fe6122d2666e
Mihajlo Eskic <eskicmihajlo55@gmail.com>
2024-05-03 15:14:17 +0200

    display icons

--- 5/app/src/main/java/com/example/a5/MainActivity.kt
+++ 5/app/src/main/java/com/example/a5/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.a5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.a5.data.Dog
 import com.example.a5.ui.theme._5Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +34,73 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun PasItem(
+    dog : Dog,
+    modifier: Modifier = Modifier
+)
+{
+
+    Card (
         modifier = modifier
     )
+    {
+        Row (modifier = modifier
+            .fillMaxSize()
+            .padding(dimensionResource(R.dimen.padding_small)))
+
+        {
+            PasIcon(pasIcon = dog.imageResouceId)
+        }
+
+    }
+
+
+
+
+
+
+
 }
 
-@Preview(showBackground = true)
+
+
+
+
+
 @Composable
-fun GreetingPreview() {
-    _5Theme {
-        Greeting("Android")
-    }
+fun PasIcon (
+    @DrawableRes pasIcon : Int,
+    modifier: Modifier = Modifier
+)
+{
+    Image(painter = painterResource(pasIcon),
+        contentDescription =null ,
+        contentScale = ContentScale.Crop,
+        modifier= modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small))
+
+
 }
+
+
+
+@Composable
+fun PasInformacije(
+    @StringRes imePsa : Int,
+    
+)
+
+
